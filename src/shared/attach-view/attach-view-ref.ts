d8eea02cedd8570cab0c2bb1f83a58ca0fe6fa05
Siang Wang <ssiangww@gmail.com>
2020-10-14 20:50:43 +0800

    remove unused code

--- src/shared/attach-view/attach-view-ref.ts
+++
@@ -1,6 +0,0 @@
-import { Observable } from 'rxjs';
-
-export class AttachViewRef<T> {
-    result: Observable<T>;
-    close: () => void;
-}
--- src/shared/modal/modal-content.component.ts
+++
@@ -1,7 +0,0 @@
-export interface ModalContentComponent {
-    /** instance data of content in modal */
-    instaData: any;
-    /** result data after clicking action button */
-    resultData: any;
-    valid?: boolean;
-}
